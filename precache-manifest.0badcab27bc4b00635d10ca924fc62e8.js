a9d93de31e7a0474f303ba9e9fac1a9e14b39a52
Sofiqul Islam <schoolwab@gmail.com>
2020-02-25 08:43:32 +0600

    Updates

---
+++ precache-manifest.0badcab27bc4b00635d10ca924fc62e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1a7afe0055fbd8af05a456eafa38472",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "e80ff62d18b4bd8ddaf6",
+    "url": "/weather/static/css/main.494a9cd9.chunk.css"
+  },
+  {
+    "revision": "46b6154d25e10574d45b",
+    "url": "/weather/static/js/2.a7d80164.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/weather/static/js/2.a7d80164.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e80ff62d18b4bd8ddaf6",
+    "url": "/weather/static/js/main.4f01e919.chunk.js"
+  },
+  {
+    "revision": "02f80e29b5552fb91c48",
+    "url": "/weather/static/js/runtime-main.f075ed78.js"
+  }
+]);
